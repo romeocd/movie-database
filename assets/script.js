ec9c68eb496ef93c53c20b4fd0bd5b2f61a1b397
SumailaM <142268062+SumailaM@users.noreply.github.com>
2023-10-05 18:43:13 -0700

    RottenApi

--- assets/script.js
+++ assets/script.js
@@ -3,12 +3,13 @@ var movieNameRef = document.getElementById("movie-name");
 var searchBtn = document.getElementById("search-btn");
 var result = document.getElementById("result");
 var key = "b56525c";
+var movieName;
 
 var movieNameInput = document.getElementById('movie-name');
 
 //fetch data from api
 var getMovie = function () {
-    var movieName = movieNameRef.value.trim(); // Trim whitespace
+     movieName = movieNameRef.value.trim(); // Trim whitespace
 
     // Check for empty input
     if (movieName.length <= 0) {
@@ -71,3 +72,25 @@ searchBtn.addEventListener("click", getMovie);
 window.addEventListener("load", getMovie);
 // Add an event listener to the input field to listen for Enter key press
 movieNameInput.addEventListener('keydown', runGetMovieOnEnter);
+
+// code for rotten tomatoes api
+document.getElementById('searchReviewsButton').addEventListener('click', function () {
+    // Rotten api key
+    const apiKey = 'https://rotten-tomatoes-api.ue.r.appspot.com';
+
+    // Define the URL for the Rotten Tomatoes API
+    const apiUrl = `https://rotten-tomatoes-api.ue.r.appspot.com/movie/${movieName}`;
+
+    // Make a GET request to the Rotten Tomatoes API
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            // Handle the API response data here
+            // For example, you can display the reviews in a div
+            const reviewsContainer = document.getElementById('reviewsContainer');
+            reviewsContainer.innerHTML = JSON.stringify(data, null, 2);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}); 
